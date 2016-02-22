a201388f40a9e0c1a7c33fae489ddc997d76d2aa
hamedelsayed <hamedelsayed@yahoo.com>
2016-02-22 19:15:42 +0200

    Customers

---
+++ CustomerService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TasktyCRM.Data;
+using TasktyCRM.Data.Infrastructure;
+
+namespace TasktyCRM.Service.CustomerService
+{
+    public class CustomerService:EntityService<Customer>, ICustomerService
+    {
+        readonly TasktyCRM.Data.TasktyCRM _context;
+        public CustomerService(TasktyCRM.Data.TasktyCRM context)
+            : base(context)
+        {
+            _context = context;
+            Dbset = _context.Set<Customer>();
+        }
+        public override IEnumerable<Customer> GetAll()
+        {
+            return _context.Customers.ToList();
+        }
+
+
+        public Customer GetById(int Id)
+        {
+            return _context.Customers.FirstOrDefault(x => x.Id == Id);
+        }
+
+    }
+}
---
+++ ICustomerService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TasktyCRM.Data;
+using TasktyCRM.Data.Infrastructure;
+
+namespace TasktyCRM.Service.CustomerService
+{
+    public interface ICustomerService:IEntityService<Customer>
+    {
+        Customer GetById(int Id);
+    }
+}
